e2ecdf2ae604b3085ada7b41aec846fa318ff4c9
RamSailopal <thelinuxguys@yahoo.com>
2021-11-29 15:24:26 +0000

    First commit

---
+++ react/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
+
+export default class App extends Component {
+          constructor(props){
+                      super(props)
+                      this.state = {
+                                    users: [],
+                                    loading:true
+                                  }
+                    }
+          async getUsersData(){
+                      const res = await axios({url: 'http://192.168.240.21:8080/http://192.168.240.21/fmqlEP?fmql=SELECT%209_6%20LIMIT%20100' })
+                      console.log(res.data.results)
+                      this.setState({loading:false, users: res.data.results})
+                    }
+          componentDidMount(){
+                      this.getUsersData()
+                    }
+          render() {
+                      const columns = [{
+                                    Header: 'fmId',
+                                    accessor: 'uri.fmId',
+                                   },
+                                   {
+                                    Header: 'fmType',
+                                    accessor: 'uri.fmType',
+                                   },
+                                   {
+                                    Header: 'Value',
+                                    accessor: 'uri.value',                                                        },
+                                   {
+                                    Header: 'Type',
+                                    accessor: 'uri.type'
+                                   },
+                                   {
+                                    Header: 'Label',
+                                    accessor: 'uri.label'
+                                   }
+                                ]
+                      return (
+                                    <ReactTable
+                                    data={this.state.users}
+                                    columns={columns}
+                                 />
+                                  )
+                    }
+}
